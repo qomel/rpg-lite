9e034c310242c44d30274caa2c5d71d39b89d66a
Dominik Pazurek <80347310+qomel@users.noreply.github.com>
2025-12-31 15:58:45 +0100

    Dodanie aliasów typów w TypeScript które reprezentują różne elementy gry, takie jak ID gracza, nazwa gracza, poziom doświadczenia i punkty zdrowia.

---
+++ src/game/types.ts
@@ -0,0 +1,40 @@
+// Zmienna typu Rarity określa rzadkość przedmiotu w grze.
+export type Rarity = "common" | "uncommon" | "rare" | "epic" | "legendary";
+
+export type Stats = {
+  maxHp: number;
+  hp: number;
+  exp: number;
+  level: number;
+  strenght: number;
+  armor: number;
+  luck: number;
+  gold: number;
+};
+
+export type Mob = {
+  id: string;
+  name: string;
+  threatLevel: number; // do obliczenia winChance
+  mobAttack: number;
+  expReward: number;
+  goldMin: number;
+  goldMax: number;
+  lootMultipler: number; // mnożnik do szansy na zdobycie przedmiotu
+};
+
+export type Item = {
+  id: string;
+  name: string;
+  rarity: Rarity;
+  sellPrice: number;
+};
+
+export type BattleResult = {
+  win: boolean;
+  damageTaken: number;
+  expGained: number;
+  goldGained: number;
+  droppedItems?: Item;
+  log: string[];
+};
